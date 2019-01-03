c84f573160c3de470b40823360a29c249b6ef871
Ben Gelens <ben@bgelens.nl>
2019-01-03 15:52:20 +0100

    working pull server

--- DSCPullaris/DSCPullaris.psm1
+++ DSCPullaris/DSCPullaris.psm1
@@ -1,25 +1,54 @@
-$cred = [pscredential]::new('ben', (ConvertTo-SecureString -String '' -AsPlainText -Force))
-$null = New-DSCPullServerAdminConnection -SQLServer '' -Credential $cred -Database dsc
-$configDirectory = 'C:\Users\BenGelens\Desktop\bla'
-$moduleDirectory = 'C:\Users\BenGelens\Desktop\modules'
+$cred = [pscredential]::new('ben', (ConvertTo-SecureString -String 'bla' -AsPlainText -Force))
+$session = New-DSCPullServerAdminConnection -SQLServer 'dscpull' -Credential $cred -Database dsc -DontStore
 
-New-PolarisPutRoute -Path 'Nodes:ID' -Scriptblock {
-    $script:Polaris.Log('Node Registration')
-    $script:Polaris.Log(($Request.Body | ConvertTo-Json -Depth 100))
+$configDirectory = 'C:\Users\Administrator\pullaris\Configurations'
+$moduleDirectory = 'C:\Users\Administrator\pullaris\Modules'
 
+function Test-DSCCLientHeader {
+    param (
+        $Request
+    )
     if ($Request.Headers['ProtocolVersion'] -ne '2.0') {
-        $Response.StatusCode = 400
+        $false
+    } else {
+        $true
+    }
+}
+
+function Set-DSCServerHeader {
+    param (
+        $Response,
+        $StatusCode,
+        [hashtable]$Headers
+    )
+    [void]$Response.Headers.Add('ProtocolVersion', '2.0')
+    [void]$Response.Headers.Add('X-Content-Type-Options', 'nosniff')
+    [void]$Response.Headers.Add('Cache-Control', 'no-cache')
+    $Response.StatusCode = $StatusCode
+
+    if ($PSBoundParameters.ContainsKey('Headers')) {
+        $Headers.GetEnumerator().ForEach{
+            [void]$Response.Headers.Add($_.Name, $_.Value)
+        }
+    }
+}
+
+New-PolarisPutRoute -Path "/api/Nodes\(AgentId=':ID'\)" -Scriptblock {
+    $agentId = $Request.Parameters.ID
+
+    if (-not (Test-DSCCLientHeader -Request $Request)) {
+        Set-DSCServerHeader -Response $Response -StatusCode 400
         $Response.Send('Client protocol version is invalid.')
     } else {
-        $agentId = ($Request.Parameters.ID -split '=')[-1].TrimEnd(')').Trim("'")
-        $existingNode = Get-DSCPullServerAdminRegistration -AgentId $agentId
+        $existingNode = Get-DSCPullServerAdminRegistration -AgentId $agentId -Connection $session
         if ($null -eq $existingNode) {
             $newArgs = @{
-                AgentId = $agentId
+                AgentId = $AgentId
                 LCMVersion = $Request.Body.AgentInformation.LCMVersion
                 NodeName = $Request.Body.AgentInformation.NodeName
                 IPAddress = $Request.Body.AgentInformation.IPAddress -split ';' -split ',' | Where-Object -FilterScript {$_ -ne [string]::Empty}
                 Confirm = $false
+                Connection = $session
             }
 
             # ReportServer registration does not contain ConfigurationNames
@@ -34,6 +63,7 @@ New-PolarisPutRoute -Path 'Nodes:ID' -Scriptblock {
                 NodeName = $Request.Body.AgentInformation.NodeName
                 IPAddress = $Request.Body.AgentInformation.IPAddress -split ';' -split ',' | Where-Object -FilterScript {$_ -ne [string]::Empty}
                 Confirm = $false
+                Connection = $session
             }
 
             # ReportServer registration does not contain ConfigurationNames
@@ -43,40 +73,26 @@ New-PolarisPutRoute -Path 'Nodes:ID' -Scriptblock {
 
             $existingNode | Set-DSCPullServerAdminRegistration @updateArgs
         }
-        $Response.StatusCode = 201
-        $Response.Headers.Add('ProtocolVersion', '2.0')
+        Set-DSCServerHeader -Response $Response -StatusCode 204
     }
 }
 
-New-PolarisPostRoute -Path 'Nodes:ID/GetDscAction' -Scriptblock {
-    $script:Polaris.Log('Get Configuration')
-    $script:Polaris.Log(($Request.Body | ConvertTo-Json -Depth 100))
-    $script:Polaris.Log(($Request.Headers | ConvertTo-Json -Depth 100))
+New-PolarisPostRoute -Path "/api/Nodes\(AgentId=':ID'\)/GetDscAction" -Scriptblock {
+    # TODO: Validate if agent is registered
+    $agentId = $Request.Parameters.ID
 
-    $agentId = ($Request.Parameters.ID -split '=')[-1].TrimEnd(')').Trim("'")
-
-    $existingNode = Get-DSCPullServerAdminRegistration -AgentId $agentId
-
-    # always respect ConfigurationName in Database? What about partial configs?
-    $filePath = (Join-Path -Path $configDirectory -ChildPath $existingNode.ConfigurationNames[0]) + '.mof'
-    $file = Get-Item -Path $filePath -ErrorAction SilentlyContinue
-
-    if ($null -eq $file) {
-        $script:Polaris.Log('Configuration Document not found, sending OK')
-        # send OK, even when file is not found
-        $responseBody = @{
-            NodeStatus = 'Ok'
-            Details = @(
-                @{
-                    ConfigurationName = $existingNode.ConfigurationNames
-                    Status = 'Ok'
-                }
-            )
-        }
+    if (-not (Test-DSCCLientHeader -Request $Request)) {
+        Set-DSCServerHeader -Response $Response -StatusCode 400
+        $Response.Send('Client protocol version is invalid.')
     } else {
-        $checksum = ($file | Get-FileHash -Algorithm SHA256).Hash
-        if ($Request.Body.ClientStatus.Checksum -eq $checksum) {
-            $script:Polaris.Log('Configuration Document checksum same as on client, sending OK')
+        $existingNode = Get-DSCPullServerAdminRegistration -AgentId $agentId -Connection $session
+
+        # always respect ConfigurationName in Database? What about partial configs?
+        $filePath = (Join-Path -Path $configDirectory -ChildPath $existingNode.ConfigurationNames[0]) + '.mof'
+        $file = Get-Item -Path $filePath -ErrorAction SilentlyContinue
+
+        if ($null -eq $file) {
+            # send OK, even when file is not found
             $responseBody = @{
                 NodeStatus = 'Ok'
                 Details = @(
@@ -87,87 +103,141 @@ New-PolarisPostRoute -Path 'Nodes:ID/GetDscAction' -Scriptblock {
                 )
             }
         } else {
-            $script:Polaris.Log('Configuration Document checksum different from on client, sending GetConfiguration')
-            $responseBody = @{
-                NodeStatus = 'GetConfiguration'
-                Details = @(
-                    @{
-                        ConfigurationName = $existingNode.ConfigurationNames
-                        Status = 'GetConfiguration'
-                    }
-                )
+            $checksum = ($file | Get-FileHash -Algorithm SHA256).Hash
+            if ($Request.Body.ClientStatus.Checksum -eq $checksum) {
+                $responseBody = @{
+                    NodeStatus = 'Ok'
+                    Details = @(
+                        @{
+                            ConfigurationName = $existingNode.ConfigurationNames
+                            Status = 'Ok'
+                        }
+                    )
+                }
+            } else {
+                $responseBody = @{
+                    NodeStatus = 'GetConfiguration'
+                    Details = @(
+                        @{
+                            ConfigurationName = $existingNode.ConfigurationNames
+                            Status = 'GetConfiguration'
+                        }
+                    )
+                }
             }
         }
+        Set-DSCServerHeader -Response $Response -StatusCode 200
+        $response.Json(($responseBody | ConvertTo-Json))
     }
-
-    $Response.StatusCode = 200
-    $Response.ContentType = 'application/json'
-    $Response.Headers.Add('ProtocolVersion', '2.0')
-    $response.Json(($responseBody | ConvertTo-Json))
 }
 
-New-PolarisGetRoute -Path 'Nodes:ID/Configurations:ConfigName/ConfigurationContent' -Scriptblock {
-    $script:Polaris.Log('Get Configuration Content')
-    $script:Polaris.Log(($Request.Body | ConvertTo-Json -Depth 100))
-    $script:Polaris.Log(($Request.Headers | ConvertTo-Json -Depth 100))
+New-PolarisGetRoute -Path "/api/Nodes\(AgentId=':ID'\)/Configurations\(ConfigurationName=':ConfigName'\)/ConfigurationContent" -Scriptblock {
+    # TODO: Validate if agent is registered
+    $agentId = $Request.Parameters.ID
+    $configName = $Request.Parameters.ConfigName
 
-    $agentId = ($Request.Parameters.ID -split '=')[-1].TrimEnd(')').Trim("'")
-    $configName = ($Request.Parameters.ConfigName -split '=')[-1].TrimEnd(')').Trim("'")
-
-    $Response.Headers.Add('ProtocolVersion', '2.0')
-
-    $filePath = (Join-Path -Path $configDirectory -ChildPath $configName) + '.mof'
-    $file = Get-Item -Path $filePath -ErrorAction SilentlyContinue
-    $script:Polaris.Log($file)
-
-    if ($null -eq $file) {
-        $Response.StatusCode = 404
+    if (-not (Test-DSCCLientHeader -Request $Request)) {
+        Set-DSCServerHeader -Response $Response -StatusCode 400
+        $Response.Send('Client protocol version is invalid.')
     } else {
-        $content = $file | Get-Content -Encoding unicode
-        $checksum = ($file | Get-FileHash -Algorithm SHA256).Hash
+        $filePath = (Join-Path -Path $configDirectory -ChildPath $configName) + '.mof'
+        $file = Get-Item -Path $filePath -ErrorAction SilentlyContinue
 
-        $fileBytes = [io.file]::ReadAllBytes($file)
+        if ($null -eq $file) {
+            Set-DSCServerHeader -StatusCode 404
+        } else {
+            $content = $file | Get-Content -Encoding unicode
+            $checksum = ($file | Get-FileHash -Algorithm SHA256).Hash
 
-        $Response.StatusCode = 200
-        $Response.ContentType = 'application/octet-stream'
-        $Response.Headers.Add('Content-Length', $content.Length)
-        $Response.Headers.Add('Checksum', $checkSum)
-        $Response.Headers.Add('ChecksumAlgorithm','SHA-256')
-        $Response.ByteResponse = $fileBytes
+            $fileBytes = [io.file]::ReadAllBytes($file)
+
+            Set-DSCServerHeader -Response $Response -StatusCode 200 -Headers @{
+                'Content-Length' = $content.Length
+                'Checksum' = $checkSum
+                'ChecksumAlgorithm' = 'SHA-256'
+            }
+
+            $Response.ContentType = 'application/octet-stream'
+            $Response.ByteResponse = $fileBytes
+        }
     }
 }
 
-New-PolarisGetRoute -Path 'Modules:ModuleName,:ModuleVersion/ModuleContent' -Scriptblock {
-    $script:Polaris.Log('Get Module Content')
-    $script:Polaris.Log(($Request.Body | ConvertTo-Json -Depth 100))
-    $script:Polaris.Log(($Request.Headers | ConvertTo-Json -Depth 100))
-    $script:Polaris.Log(($Request.Parameters | ConvertTo-Json -Depth 100))
+New-PolarisGetRoute -Path "/api/Modules\(ModuleName=':ModuleName',ModuleVersion=':ModuleVersion'\)/ModuleContent" -Scriptblock {
+    # TODO: Validate if agent is registered
+    $agentId = $Request.Headers['AgentId']
+    $moduleName = $Request.Parameters.ModuleName
+    $moduleVersion = $Request.Parameters.ModuleVersion
 
-    $moduleName = ($Request.Parameters.ModuleName -split '=')[-1].TrimEnd(')').Trim("'")
-    $moduleVersion = ($Request.Parameters.ModuleVersion -split '=')[-1].TrimEnd(')').Trim("'")
-
-    $Response.Headers.Add('ProtocolVersion', '2.0')
-
-    $moduleFullName = $moduleName + '_' + $moduleVersion + '.zip'
-    $filePath = Join-Path -Path $moduleDirectory -ChildPath $moduleFullName
-    $file = Get-Item -Path $filePath -ErrorAction SilentlyContinue
-
-    $script:Polaris.Log($file)
-    if ($null -eq $file) {
-        $Response.StatusCode = 404
+    if (-not (Test-DSCCLientHeader -Request $Request)) {
+        Set-DSCServerHeader -Response $Response -StatusCode 400
+        $Response.Send('Client protocol version is invalid.')
     } else {
-        $content = $file | Get-Content -Encoding unicode
-        $checksum = ($file | Get-FileHash -Algorithm SHA256).Hash
+        $moduleFullName = $moduleName + '_' + $moduleVersion + '.zip'
+        $filePath = Join-Path -Path $moduleDirectory -ChildPath $moduleFullName
+        $file = Get-Item -Path $filePath -ErrorAction SilentlyContinue
 
-        $fileBytes = [io.file]::ReadAllBytes($file)
+        if ($null -eq $file) {
+            Set-DSCServerHeader -StatusCode 404
+        } else {
+            $content = $file | Get-Content -Encoding unicode
+            $checksum = ($file | Get-FileHash -Algorithm SHA256).Hash
 
-        $Response.StatusCode = 200
-        $Response.ContentType = 'application/octet-stream'
-        $Response.Headers.Add('Content-Length', $content.Length)
-        $Response.Headers.Add('Checksum', $checkSum)
-        $Response.Headers.Add('ChecksumAlgorithm','SHA-256')
-        $Response.ByteResponse = $fileBytes
+            $fileBytes = [io.file]::ReadAllBytes($file)
+
+            Set-DSCServerHeader -Response $Response -StatusCode 200 -Headers @{
+                'Content-Length' = $content.Length
+                'Checksum' = $checkSum
+                'ChecksumAlgorithm' = 'SHA-256'
+            }
+
+            $Response.ContentType = 'application/octet-stream'
+            $Response.ByteResponse = $fileBytes
+        }
     }
 }
 
-$null = Start-Polaris -Port 8081 -UseJsonBodyParserMiddleware
+New-PolarisPostRoute -Path "/api/Nodes\(AgentId=':ID'\)/SendReport" -Scriptblock {
+    # TODO: Validate if agent is registered
+    $agentId = $Request.Parameters.ID
+    $jobId = $Request.Body.JobId
+
+    if (-not (Test-DSCCLientHeader -Request $Request)) {
+        Set-DSCServerHeader -Response $Response -StatusCode 400
+        $Response.Send('Client protocol version is invalid.')
+    } else {
+        $reportArgs = @{}
+        $Request.Body.psobject.properties.ForEach{ $reportArgs[$_.Name] = $_.Value }
+
+        if ($reportArgs.ContainsKey('IPAddress')) {
+            $reportArgs.IPAddress = $reportArgs.IPAddress -split ';' -split ',' | Where-Object -FilterScript {$_ -ne [string]::Empty}
+        }
+
+        if ($reportArgs.ContainsKey('RebootRequested')) {
+            $reportArgs.RebootRequested = $reportArgs.RebootRequested.ToBoolean($_)
+        }
+
+        $script:Polaris.Log(($reportArgs | Out-String))
+        $existingReport = Get-DSCPullServerAdminStatusReport -JobId $jobId -AgentId $agentId -Connection $session
+        if ($null -eq $existingReport) {
+            $script:Polaris.Log('Creating new Report')
+            New-DSCPullServerAdminStatusReport @reportArgs -Id $agentId -Connection $session -Confirm:$false
+        } else {
+            $script:Polaris.Log('Updating Existing Report')
+            $reportArgs.GetEnumerator().ForEach{
+                $existingReport."$($_.Name)" = $_.value
+            }
+            $existingReport | Set-DSCPullServerAdminStatusReport -Connection $session -Confirm:$false
+        }
+
+        Set-DSCServerHeader -Response $Response -StatusCode 200
+    }
+}
+
+$pol = Get-Polaris
+$pol.Logger = {
+    param($LogItem)
+    Write-Host $LogItem
+}
+
+$null = Start-Polaris -Port 8081 -UseJsonBodyParserMiddleware -Verbose
